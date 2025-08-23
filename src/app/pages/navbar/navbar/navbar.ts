e7239eb82ccac435358d541e292ebc27b852626d
AngelFaciano <randrea.9523@gmail.com>
2025-08-23 12:43:38 -0300

    modifica nav

---
+++ src/app/pages/navbar/navbar/navbar.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [NgOptimizedImage], 
+  templateUrl: './navbar.html',
+  styleUrls: ['./navbar.css']
+})
+export class Navbar {
+  logoUrl = '/icons/baner.png'; 
+}
