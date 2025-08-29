32a8fc5faf59a13714382aa7435a2e6fa2a3e770
AngelFaciano <randrea.9523@gmail.com>
2025-08-29 18:00:20 -0300

    evoluciones

--- src/app/pages/infopoke/infopoke.component.ts
+++ src/app/pages/infopoke/infopoke.component.ts
@@ -15,13 +15,16 @@ import { HttpClient } from '@angular/common/http';
 
 export class InfopokeComponent {
   data = signal<any | null>(null);
-  p:number=0
+  p: number[] = [0, 0, 0, 0, 0, 0];
+  j:string=""
   constructor(private poke: SeachPokeService) {
     // efecto reactivo
     effect(async () => {
-      const pokeData = await this.poke.getPokemonId(1);
+      const pokeData = await this.poke.getPokemonId(150);
       this.data.set(pokeData);
-      this.p=this.data().stats[0].base_stat
+      this.j=this.data().sprites.other["official-artwork"].front_default
+      for(let i of [0,1,2,3,4,5])
+      this.p[i]=this.data().stats[i].base_stat
     });
   }
 }
